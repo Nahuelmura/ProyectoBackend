43fb5d8d53484a76b94f34181375873acb92d0b1
Nahuel Mura <nmuratore080@gmail.com>
2026-08-26 21:30:41 -0300

    cambios en controlador Post

--- ApiPedido/Controllers/CategoriaController.cs
+++ ApiPedido/Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
 using ApiPedido.Data;
+using ApiPedido.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,5 +25,35 @@ namespace ApiPedidos.Controllers
 
             return Ok(categorias);
         }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> CrearCategoria([FromBody] Categoria categoria)
+        {
+            var nombreMayuscula = categoria.Nombre?.Trim().ToUpper();
+
+            var existeCategoria = await _context.Categorias.AnyAsync(e => e.Nombre == nombreMayuscula);
+
+            if (!existeCategoria)
+            {
+                var nuevaCategoria = new Categoria
+                {
+                    Nombre = nombreMayuscula,
+                };
+                             _context.Add(nuevaCategoria);
+                await _context.SaveChangesAsync();
+                return Ok("Categoria guardada");
+            }
+
+
+            return Ok();
+        }
+
+
+
+
     }
+
+
 }
